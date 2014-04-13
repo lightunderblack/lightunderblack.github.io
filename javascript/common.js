2f9be99a24cd99559f716840e4728d86ceedd063
Shiny Bender <lightunderblack@gmail.com>
2014-04-14 00:32:20 +0800

    modify

---
+++ javascript/common.js
@@ -0,0 +1,55 @@
+(function($){
+    var top,
+        extractHeight,
+        $go2Top = $('.go2Top'),
+        $navigation = $('.navigation'),
+        $sidenavWrapper = $('.sidenav-wrapper'),
+        sidenavWrapper = $sidenavWrapper[0];
+    
+    function getHeight(){
+        extractHeight = $navigation.outerHeight(true);
+        top = -($sidenavWrapper.outerHeight(true)+extractHeight)+'px';
+    }
+
+    getHeight();
+
+	//初始化
+    new SmoothScroll({
+    	duration: 300,
+        go2Top: $go2Top,
+        container: $(window),
+        destination: $('body')
+    });
+    
+    $go2Top.on('click', function(){
+        if($sidenavWrapper.css('visibility') === 'visible'){
+            $sidenavWrapper.css('top', top);    
+        }
+    });  	
+    $navigation.children('.menu').on('click', function(e){
+    	if($sidenavWrapper.css('visibility') === 'hidden'){
+    		$sidenavWrapper.css({
+    			visibility: 'visible',
+    			top: extractHeight + 'px'
+    		});    		
+    	}else{
+			$sidenavWrapper.css({top: top});	
+    	}
+    	e.stopPropagation();
+    });
+    $(document).on('click', function(e){
+    	var target = e.srcElement || e.target;
+    	if($sidenavWrapper.css('visibility') === 'visible'){
+    		$sidenavWrapper.css('top', top);	
+    	}
+    });
+    $sidenavWrapper.on('webkitTransitionEnd', function(e){
+    	var $this = $(this);
+    	if(parseInt($this.css('top'), 10) < 0){
+    		$this.css({visibility: 'hidden'});
+    	}
+    });
+    window.onorientationchange = window.onresize = function(){
+        getHeight(); 
+    };
+})(jQuery);
